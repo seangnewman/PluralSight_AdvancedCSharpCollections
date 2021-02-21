53eaa1902fafe64bc069c28c43cb17c418e17f80
seangnewman <seangnewman@msn.com>
2021-02-21 13:05:04 -0700

    Introduction TourBook and review of O(n) for collections

---
+++ TourBooker.Logic/AppData.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Pluralsight.AdvCShColls.TourBooker.Logic
+{
+	public class AppData
+	{
+		public List<Country> AllCountries { get; private set; }
+		public void Initialize(string csvFilePath)
+		{
+			CsvReader reader = new CsvReader(csvFilePath);
+            #region Sorting List.Sort vs LINQ
+            //this.AllCountries = reader.ReadAllCountries();
+            this.AllCountries = reader.ReadAllCountries().OrderBy(n => n.Name).ToList();
+            #endregion
+
+
+        }
+    }
+}
--- TourBooker.Logic/Properties/AssemblyInfo.cs
+++ TourBooker.Logic/Properties/AssemblyInfo.cs
@@ -5,12 +5,12 @@ using System.Runtime.InteropServices;
 // General Information about an assembly is controlled through the following
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
-[assembly: AssemblyTitle("PluralSight_AdvancedCSharpCollections")]
+[assembly: AssemblyTitle("TourBooker.Logic")]
 [assembly: AssemblyDescription("")]
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("")]
-[assembly: AssemblyProduct("PluralSight_AdvancedCSharpCollections")]
-[assembly: AssemblyCopyright("Copyright ©  2021")]
+[assembly: AssemblyProduct("TourBooker.Logic")]
+[assembly: AssemblyCopyright("Copyright ©  2019")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
 
@@ -20,7 +20,7 @@ using System.Runtime.InteropServices;
 [assembly: ComVisible(false)]
 
 // The following GUID is for the ID of the typelib if this project is exposed to COM
-[assembly: Guid("c98ce8c0-6090-4e63-a1dd-def18749e146")]
+[assembly: Guid("77bb6516-4e38-4461-8ccd-02c12ff7d0e2")]
 
 // Version information for an assembly consists of the following four values:
 //
